d3a6a08ee04fed4b833e24c4d4c4572ca5a57cf0
Aleksander Dudek <aleksander.dudek@rallyhealth.com>
2023-09-07 23:41:00 +0200

    added jwt handling

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const Jwt = require("jsonwebtoken");
+const jwtKey = "e-company";
 
 require("./db/config");
 
@@ -12,23 +15,49 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/register", async (req, resp) => {
-  let user = new User(req.body);
+  // hash password
+  let user = new User({
+    ...req.body,
+    password: await bcrypt.hash(req.body.password, 12),
+  });
+
   let result = await user.save();
   //remove password from returned object
   result = result.toObject();
   delete result.password;
-  resp.send(result);
+
+  //jwt signing
+  Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+    if (err) {
+      resp.send("Something went wrong with authentication");
+    }
+    resp.send({ result, auth: token });
+  });
+
+  // resp.send(result);
 });
 
+// this returns prebious req.body without password + auth
 app.post("/login", async (req, resp) => {
   // validate correct authentication
   if (!req.body.password || !req.body.email)
     return resp.send({ result: "No User found" });
 
-  // remove password from object
-  let user = await User.findOne(req.body).select("-password");
-  if (user) {
-    resp.send(user);
+  let result = await User.findOne({ email: req.body.email });
+
+  if (await bcrypt.compare(req.body.password, result.password)) {
+    // now we can remove password from object
+    delete result.password;
+
+    //jwt sign
+    Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+      if (err) {
+        resp.send("Something went wrong with authentication");
+      }
+      resp.send({ result, auth: token });
+    });
+
+    // resp.send(user);
   } else {
     resp.send({ result: "No User found" });
   }
@@ -72,7 +101,48 @@ app.get("/get-product/:id", async (req, resp) => {
   }
 });
 
-app.get("/search-product/:key", async (req, resp) => {});
+app.get("/search-product/:key", verifyToken, async (req, resp) => {
+  // it is searching for either of the properties described by key,
+  // if I add another property within the JS object then it is AND within
+  let result = await Product.find({
+    $or: [
+      {
+        name: { $regex: req.params.key },
+        // company: { $regex: req.params.key },
+      },
+      {
+        company: { $regex: req.params.key },
+      },
+    ],
+  });
+  resp.send(result);
+});
+
+//middleware
+function verifyToken(req, resp, next) {
+  console.warn(req.header["authorization"]);
+  let token = req.headers["authorization"];
+
+  // this just checks if we have token present
+  // how about having correct token?
+  if (token) {
+    // get part after bearer
+    token = token.split(" ")[1];
+    console.warn(token);
+    Jwt.verify(token, jwtKey, (err, valid) => {
+      if (err) {
+        resp.status(401).send({ result: "Please provide a valid token" });
+      } else {
+        //if fine it redirects the response
+        next();
+      }
+    });
+  } else {
+    resp.send("Please provide a token");
+  }
+  //if fine it redirects the response IF now JWT
+  // next();
+}
 
 // which port to use
 app.listen(5005);
